9be6cfe6803f175621065c207a92657172a3cab3
SM-tek <96948516+smtekdev@users.noreply.github.com>
2023-04-17 23:03:17 +0500

    complete

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,42 @@
+
+<a class="navbar-brand" href="{{route("index")}}" style="position: relative; width: 12rem; height: 5rem; margin-left: 40%; z-index: 5; margin-top: 1.5%;">
+    <img src="agency-studio/img/spark black.png" alt="logo" class="logo-default">
+    <!-- <img src="agency-studio/img/white spark.png" alt="logo" class="logo-scrolled" style="position: relative; width: 12rem; height: 5rem; margin-left: 40%;"> -->
+</a>
+
+
+<!-- new Navbar -->
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style="margin-top: -1.5%; margin-bottom: 1%; background-color: transparent !important;">
+    <div class="container">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item">
+          <a class="nav-link2" href="{{route("index")}}" style="font-weight: 500;">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link2" href="{{("about")}}" style="font-weight: 500;">About</a>
+        </li>
+        <li class="nav-item" style="width: 7rem;">
+          <a class="nav-link2" href="{{("portfolio")}}" style="font-weight: 500;">Portfolio</a>
+        </li>
+        <li class="nav-item" style="width: 13rem;">
+          <a class="nav-link2" href="{{("services")}}" style="font-weight: 500;">Website Design</a>
+        </li>
+        <li class="nav-item" style="width: 14rem;">
+          <a class="nav-link2" href="{{("app-development")}}" style="font-weight: 500;">App Development</a>
+        </li>
+        <li class="nav-item" style="width: 10rem;">
+          <a class="nav-link2" href="{{route("design")}}" style="font-weight: 500;">UI/UX Design</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link2" href="{{("seo")}}" style="font-weight: 500;">SEO</a>
+        </li>
+        <li class="nav-item" style="width: 10rem;">
+            <a class="nav-link2" href="{{("contactForm")}}" style="font-weight: 500;">Contact Us</a>
+        </li>
+        
+      </ul>
+    </div>
+  </nav>
+
+<!-- new Navbar End -->
